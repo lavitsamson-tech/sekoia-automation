f4a433f33bbcef07aa308f988c2e0e4b04f23be1
Lavit Samson <lavit.samson@locaterisk.com>
2026-05-22 13:25:57 +0200

    modified the module configuration and did linting

--- LocateRisk/locaterisk_modules/models.py
+++ LocateRisk/locaterisk_modules/models.py
@@ -2,6 +2,6 @@ from pydantic import BaseModel, Field
 
 
 class LocateriskModuleConfiguration(BaseModel):
-    api_key: str = Field(..., description="API Key", secret=True)
-    scan_id: str = Field(..., description="Scan ID", secret=True)
+    api_key: str = Field(..., description="API Key", json_schema_extra={"secret": True})
+    scan_id: str = Field(..., description="Scan ID", json_schema_extra={"secret": True})
     report_url: str = "https://app.locaterisk.com/api/rest/report/export"
--- LocateRisk/main.py
+++ LocateRisk/main.py
@@ -2,7 +2,6 @@ from locaterisk_modules import LocateriskModule
 
 from locaterisk_modules.connector import LocateriskConnector
 
-
 if __name__ == "__main__":
     module = LocateriskModule()
     module.register(LocateriskConnector, "LocateriskConnector")
